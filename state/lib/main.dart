0c7d628886d726b227cd0ca295d1660d36dc92a1
Muhammad Faizan Aslam <138019714+FaiziBytes@users.noreply.github.com>
2025-06-04 22:06:33 +0500

    Second method of the Provider

--- state/lib/main.dart
+++ state/lib/main.dart
@@ -1,12 +1,72 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'State.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'State.dart';
+//
+// void main() {
+//   runApp(ChangeNotifierProvider(
+//     create: (context) => Counter(),
+//     child: MyApp(),
+//   ));
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     print("build");
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.blue,
+//           foregroundColor: Colors.white,
+//           centerTitle: true,
+//           title: Text("State Management"),
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text("You have pushed this button many times"),
+//               SizedBox(
+//                 height: 5,
+//               ),
+//               Consumer<Counter>(builder: (context, counter, child) {
+//                 return Text(
+//                   "${counter.count}",
+//                   style: TextStyle(color: Colors.blue, fontSize: 24),
+//                 );
+//               }),
+//             ],
+//           ),
+//         ),
+//         floatingActionButton:
+//             Consumer<Counter>(builder: (context, counter, child) {
+//           return FloatingActionButton(
+//             backgroundColor: Colors.blue,
+//             foregroundColor: Colors.white,
+//             onPressed: counter.increment,
+//             child: Icon(Icons.add),
+//           );
+//         }),
+//       ),
+//     );
+//   }
+// }
+
+// Second Method
+
+import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+import "State.dart";
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => Counter(),
-    child: MyApp(),
-  ));
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Counter(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,41 +74,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("build");
+    print("Build"); // This should only print ONCE
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-          centerTitle: true,
-          title: Text("State Management"),
-        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("You have pushed this button many times"),
-              SizedBox(
-                height: 5,
-              ),
+              // Consumer<Counter>(
+              //   builder: (context, counter, child) => Text(
+              //     "${counter.count}",
+              //     style: TextStyle(fontSize: 40),
+              //   ),
+              // ),
               Consumer<Counter>(builder: (context, counter, child) {
                 return Text(
                   "${counter.count}",
-                  style: TextStyle(color: Colors.blue, fontSize: 24),
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
                 );
               }),
+              SizedBox(height: 10),
             ],
           ),
         ),
-        floatingActionButton:
-            Consumer<Counter>(builder: (context, counter, child) {
-          return FloatingActionButton(
-            backgroundColor: Colors.blue,
-            foregroundColor: Colors.white,
-            onPressed: counter.increment,
-            child: Icon(Icons.add),
-          );
-        }),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.read<Counter>().increment(); // 🔥 No rebuild here
+          },
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
