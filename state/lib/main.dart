468824fa32982dd63260a40aca6f7c049e4e92fc
Muhammad Faizan Aslam <138019714+FaiziBytes@users.noreply.github.com>
2025-06-04 22:43:36 +0500

    Updated the Second method

--- state/lib/main.dart
+++ state/lib/main.dart
@@ -1,5 +1,49 @@
+// import 'package:provider/provider.dart';
+// import 'package:flutter/material.dart';
+// import 'State.dart';
+//
+// void main() {
+//   runApp(ChangeNotifierProvider(
+//     create: (context) => Counter(),
+//     child: MyApp(),
+//   ));
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     print("build");
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Consumer<Counter>(builder: (context, counter, child) {
+//             return Text(
+//               "${counter.count}",
+//               style: TextStyle(
+//                 fontSize: 40,
+//              ),
+//             );
+//           }),
+//         ),
+//         floatingActionButton:
+//             Consumer<Counter>(builder: (context, counter, child) {
+//           return FloatingActionButton(
+//             onPressed: counter.increment,
+//             child: Icon(Icons.add),
+//           );
+//         }),
+//       ),
+//     );
+//   }
+// }
+
+// Second Method
+//
+import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
-import 'package:flutter/material.dart';
 import 'State.dart';
 
 void main() {
@@ -22,78 +66,17 @@ class MyApp extends StatelessWidget {
           child: Consumer<Counter>(builder: (context, counter, child) {
             return Text(
               "${counter.count}",
-              style: TextStyle(
-                fontSize: 40,
-              ),
+              style: TextStyle(fontSize: 30),
             );
           }),
         ),
-        floatingActionButton:
-            Consumer<Counter>(builder: (context, counter, child) {
-          return FloatingActionButton(
-            onPressed: counter.increment,
-            child: Icon(Icons.add),
-          );
-        }),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.read<Counter>().increment();
+          },
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
 }
-
-// Second Method
-//
-// import "package:flutter/material.dart";
-// import "package:provider/provider.dart";
-// import "State.dart";
-//
-// void main() {
-//   runApp(
-//     ChangeNotifierProvider(
-//       create: (context) => Counter(),
-//       child: MyApp(),
-//     ),
-//   );
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     print("Build"); // This should only print ONCE
-//
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               // Consumer<Counter>(
-//               //   builder: (context, counter, child) => Text(
-//               //     "${counter.count}",
-//               //     style: TextStyle(fontSize: 40),
-//               //   ),
-//               // ),
-//               Consumer<Counter>(builder: (context, counter, child) {
-//                 return Text(
-//                   "${counter.count}",
-//                   style: TextStyle(
-//                     fontSize: 30,
-//                   ),
-//                 );
-//               }),
-//               SizedBox(height: 10),
-//             ],
-//           ),
-//         ),
-//         floatingActionButton: FloatingActionButton(
-//           onPressed: () {
-//             context.read<Counter>().increment(); // 🔥 No rebuild here
-//           },
-//           child: Icon(Icons.add),
-//         ),
-//       ),
-//     );
-//   }
-// }
